87a0963b7fbc6a9aba4f49fb5b2c1a28abb8e529
Verisana <1902Grand@gmail.com>
2018-03-29 22:19:38 +0500

    Slight changes in model

--- ad_bot/models.py
+++ ad_bot/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from profiles.models import APIKeyModel 
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class AdBotModel(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    api_keys = models.ForeignKey('profiles.APIKeyModel', on_delete=models.CASCADE)
+    stop_price = models.DecimalField(max_digits=9, decimal_places=2)
+    step = models.IntegerField()
+    volume = models.IntegerField(null=True)
+    switch = models.BooleanField(default=False)
+    frequency = models.DurationField()
+    payment_method = models.CharField(max_length=32)
+    ad_id = models.IntegerField()
+    trade_direction = models.CharField(max_length=32,
+                                       choices = (
+                                            ('sell-bitcoins-online',
+                                             'Встать на покупку'),
+                                            ('buy-bitcoins-online',
+                                             'Встать на продажу'),
+                                       ))
+
+
+class ActionLogModel(models.Model):
+    action = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+    request_method = models.CharField(max_length=10)
+    request_url = models.URLField()
+    response_data = JSONField()
+    response_code = models.IntegerField()
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser 
+
 
 class Profile(AbstractUser):
     email_confirmed = models.BooleanField(default=False)
+    api_key = models.ForeignKey('profiles.APIKeyModel', on_delete=models.CASCADE,
+                                blank=True, null=True)
+
+class APIKeyModel(models.Model):
+    name = models.CharField(max_length=64)
+    api_key = models.CharField(max_length=32)
+    api_secret = models.CharField(max_length=64)
+    username = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
